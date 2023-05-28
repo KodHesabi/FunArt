222a0bed6e3c73d37e7f7897d5648c4f7bcee192
tkursat184@gmail.com <tkursat184@gmail.com>
2023-05-28 18:01:45 +0300

    sayac eklendi

---
+++ main.js
@@ -0,0 +1,41 @@
+var btn1 = document.querySelector("#btn1")
+var btn2  = document.querySelector("#btn2")
+var btn3  = document.querySelector("#btn3")
+var btn4  = document.querySelector("#btn4")
+var btn5  = document.querySelector("#btn5")
+var sayitut=document.querySelector("#sayitut")
+
+btn1.onclick = function(){
+    var sayi=Number(sayitut.innerHTML)
+    sayitut.innerHTML=++sayi
+    console.log(sayi)
+}
+
+
+btn2.onclick = function(){
+    var sayi=Number(sayitut.innerHTML)
+    sayitut.innerHTML=++sayi
+    console.log(sayi)
+}
+
+
+btn3.onclick = function(){
+    var sayi=Number(sayitut.innerHTML)
+    sayitut.innerHTML=++sayi
+    console.log(sayi)
+}
+
+
+btn4.onclick = function(){
+    var sayi=Number(sayitut.innerHTML)
+    sayitut.innerHTML=++sayi
+    console.log(sayi)
+}
+
+
+btn5.onclick = function(){
+    var sayi=Number(sayitut.innerHTML)
+    sayitut.innerHTML=++sayi
+    console.log(sayi)
+}
+
