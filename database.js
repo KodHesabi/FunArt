9abd7fda4881ff60eb5233ec6cc81281078a431e
tkursat184@gmail.com <tkursat184@gmail.com>
2023-06-05 13:27:50 +0300

    renk ayarlamaları

--- database.js
+++ database.js
@@ -5,10 +5,14 @@ var baglanti = mysql.createConnection({
   host : 'localhost',
   user : 'root',
   password : '',
+  database: "test",
   
 });
 
-baglanti.connect(function(err){
-  if(err) throw err;
-  console.log("Baglanti Kuruldu")
-})
+baglanti.connect (function(err){
+  if(err) throw err; 
+  baglanti.query("CREATE TABLE musteriler (name VARCHAR(30), adres VARCHAR(35) ) ", function(err){
+    /* if(err) throw err; */
+    console.log("Tablo Oluşturuldu")
+  })
+}) 
--- main.js
+++ main.js
@@ -453,6 +453,25 @@ btn14.addEventListener("click", (e) => {
   btn14_sesi.currentTime = 0;
 });
 
+
+var mysql = require('mysql')
+
+var baglanti = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '',
+  database: "test",
+  
+});
+
+baglanti.connect (function(err){
+  if(err) throw err; 
+  baglanti.query("CREATE TABLE musteriler (name VARCHAR(30), adres VARCHAR(35) ) ", function(err){
+    /* if(err) throw err; */
+    console.log("Tablo Oluşturuldu")
+  })
+}) 
+
 /*
 var data=[]
 
